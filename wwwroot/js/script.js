0d8e1a09bf092f0ed41b9ebb2793452281e7fe9e
Abhinaya Muthukumar <Abhinaya Muthukumar@LAPTOP-MIK6JAS5>
2024-10-08 14:17:35 +0530

    Add project files.

---
+++ wwwroot/js/script.js
@@ -0,0 +1,16 @@
+﻿// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.image-box');
+
+    images.forEach(image => {
+        image.addEventListener('click', function () {
+            const overlay = this.querySelector('.overlay');
+            if (overlay.style.opacity === '1') {
+                overlay.style.opacity = '0';
+            } else {
+                overlay.style.opacity = '1';
+            }
+        });
+    });
+});
+
